dfc1a8bb339b5c4acab5c52c25d56bddd3eba737
Aleks Regulski <aleks.regulski@icloud.com>
2026-06-19 22:12:28 +0200

    feat: add testing job to the deploy workflow, add the grant apply logic to the docker testing db script

--- docker/database/init-testing-database.sh
+++ docker/database/init-testing-database.sh
@@ -6,3 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE system_project_testing;
     GRANT ALL PRIVILEGES ON DATABASE system_project_testing TO system_project_testing;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "system_project_testing" <<-EOSQL
+    GRANT ALL ON SCHEMA public TO system_project_testing;
+EOSQL
